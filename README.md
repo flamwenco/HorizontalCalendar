a5da635e6398462a9aa4b94ab2e5db7bbf956065
Daniel Muckerman <danielmuckerman@me.com>
2022-05-16 19:42:27 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HorizontalCalendar
+
+A description of this package.
---
+++ Tests/HorizontalCalendarTests/HorizontalCalendarTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import HorizontalCalendar
+
+final class HorizontalCalendarTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(HorizontalCalendar().text, "Hello, World!")
+    }
+}
